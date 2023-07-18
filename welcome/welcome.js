2e90efd7771492871bf56dfee423e6498cd9423a
Riccardo Spadon <spadon.riccardo@gmail.com>
2023-07-18 20:33:23 +0200

    chages

--- welcome/welcome.js
+++ welcome/welcome.js
@@ -1,3 +1,18 @@
-function proceedUnlock() {
-    console.log('proceed to quiz!??')
+const button = document.querySelector('button')
+
+window.onload = function buttonDisabled() {
+    button.disabled = true
 }
+button.addEventListener('click', buttonDisabled)
+
+
+
+function proceedUnlock() {
+    const checkbox = document.querySelector('input')
+    if(checkbox.checked) {
+        button.disabled = false
+    } else{
+        button.disabled = true
+    }
+}
+button.addEventListener('click', proceedUnlock)
